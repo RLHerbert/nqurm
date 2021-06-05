ccf18008d5115bc6187772d6928c493850009203
Rowan Herbert <rlherbert.dev@gmail.com>
2021-06-04 17:34:39 -0700

    use Instruction::*

--- src/urm/primitives.rs
+++ src/urm/primitives.rs
@@ -1,9 +1,9 @@
-use super::{EXIT, I, URM};
+use super::{Instruction::*, EXIT, URM};
 
 impl URM {
     pub fn add(lhs: usize, rhs: usize) -> Self {
         Self {
-            instructions: vec![I::J(1, 2, EXIT), I::S(0), I::S(2), I::J(0, 0, 0)],
+            instructions: vec![J(1, 2, EXIT), S(0), S(2), J(0, 0, 0)],
             registers: vec![(0, lhs), (1, rhs)].into_iter().collect(),
             ..URM::default()
         }
